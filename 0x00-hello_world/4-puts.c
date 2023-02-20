4f88ec9a3445b590b0788368b2edbddc8e79969e
Berzi96 <bpipes96@gmail.com>
2023-02-20 15:34:05 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that pronts a line with puts function
+ * Return:0(success)
+ */
+int main void(0)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
